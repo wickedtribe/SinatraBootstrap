566be5ef0fe725386ac8608c68791e7b00fa6aa7
Dezmond Finney <dezmondfinney@dezmondfinney-laptop.(none)>
2010-02-23 15:36:30 -0500

    First Commit

---
+++ app.rb
@@ -0,0 +1,38 @@
+# Load before Sinatra
+require 'rubygems'
+require 'compass' # must be loaded before sinatra
+
+# Load Sinatra
+require 'sinatra'
+require 'lib/render_partial'
+
+# Load after Sinatra
+require 'haml' # must be loaded after sinatra
+
+# Set Sinatra's variables
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, 'views'
+set :public, 'public'
+
+# Configure Compass
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
+end
+
+# At a minimum the main sass file must reside within the views directory
+# We create /views/stylesheets where all our sass files can safely reside
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
+
+get '/about' do
+  @title = 'About'
+  haml :about, :layout => :'layouts/application'
+end
+
+get '/' do
+  @title = 'Home'
+  haml :index, :layout => :'layouts/application'
+end
